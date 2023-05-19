487976bc1b8e4c2eacae342d282d6d512058cb37
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-05-19 11:21:27 +0300

    0007: 3.6.2.find_all_using_iter.py

---
+++ 3.ReModule/3.6.finditer/3.6.1.finditer_demo.py
@@ -0,0 +1,11 @@
+import re
+
+pattern = r'\d{3}'
+string = 'abc 123 def 456 fed 321 cba'
+
+result = re.finditer(pattern, string, 0)
+
+print(len(list(result)))
+
+for i in result:
+    print(i.group())

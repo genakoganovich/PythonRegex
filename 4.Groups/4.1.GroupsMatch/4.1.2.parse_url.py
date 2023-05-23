ea316c3e38e304db5503738e6840b29670016a74
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-05-23 23:47:05 +0300

    0022: parse url in progress

--- 4.Groups/4.1.GroupsMatch/4.1.2.parse_url.py
+++ 4.Groups/4.1.GroupsMatch/4.1.2.parse_url.py
@@ -3,7 +3,7 @@ import util_4_1
 
 
 def build_regex():
-    return r'<p[^<]*?>(.+?)</p>'
+    return r'\b(https?)://([a-z.]+)([a-z0-9-_/]+)(\?[a-z=&0-9]+)?(#[a-z]+)?\b'
 
 
 # util_4_1.test('4.1.1.input.txt', build_regex)

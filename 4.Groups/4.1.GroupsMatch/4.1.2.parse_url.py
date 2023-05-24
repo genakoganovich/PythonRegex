bd57ab34fcfe9a88e8e0506a21ba4d4754085b6b
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-05-24 20:32:09 +0300

    0024: parse url done

--- 4.Groups/4.1.GroupsMatch/4.1.2.parse_url.py
+++ 4.Groups/4.1.GroupsMatch/4.1.2.parse_url.py
@@ -3,7 +3,7 @@ import util_4_1
 
 
 def make_regex():
-    return r'(https?)://([a-z.]+)[a-z0-9-_/]+(\?[a-z=&0-9]+)*(#[a-z]+)*'
+    return r'(?:(?<=\s)|(?<=^))(https?)://([a-z.]+)[a-z0-9-_/]+(\?[a-z=&0-9]+)*(#[a-z]+)*(?=\s|$)'
 
 
 def make_result(found):
@@ -21,5 +21,6 @@ def make_found(build_regex_func, line):
     return list(re.finditer(build_regex_func(), line, 0))
 
 
-list(map(lambda x: print(x), make_result(make_found(make_regex, input()))))
-# util_4_1.test('4.1.2.input.txt', make_result, make_found)
+# list(map(lambda x: print(x), make_result(make_found(make_regex, input()))))
+util_4_1.test('4.1.2.input.txt', make_result, make_found, make_regex)
+

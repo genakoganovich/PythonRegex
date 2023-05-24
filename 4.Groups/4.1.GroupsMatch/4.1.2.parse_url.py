dc7d846700c61169fe9092ba8d5f2af531d2ab41
gennady <genakoganovich@gmail.com>
2023-05-24 17:19:49 +0300

    0023: 4.1.2.parse_url.py in progress 2

--- 4.Groups/4.1.GroupsMatch/4.1.2.parse_url.py
+++ 4.Groups/4.1.GroupsMatch/4.1.2.parse_url.py
@@ -2,10 +2,24 @@ import re
 import util_4_1
 
 
-def build_regex():
-    return r'\b(https?)://([a-z.]+)([a-z0-9-_/]+)(\?[a-z=&0-9]+)?(#[a-z]+)?\b'
+def make_regex():
+    return r'(https?)://([a-z.]+)[a-z0-9-_/]+(\?[a-z=&0-9]+)*(#[a-z]+)*'
 
 
-# util_4_1.test('4.1.1.input.txt', build_regex)
-list(map(lambda x: print(x), re.findall(build_regex(), input())))
+def make_result(found):
+    headers = ['Полная ссылка', 'Протокол', 'Домен', 'Параметры', 'Якорь']
+    result = list()
+    for i in range(len(found)):
+        result.append(f'{headers[0]}: {found[i].group(0)}')
+        result.append(' | '.join([f'{headers[j]}: {found[i].group(j)}' for j in range(1, len(headers))]))
+        if i < len(found) - 1:
+            result.append('')
+    return result
 
+
+def make_found(build_regex_func, line):
+    return list(re.finditer(build_regex_func(), line, 0))
+
+
+list(map(lambda x: print(x), make_result(make_found(make_regex, input()))))
+# util_4_1.test('4.1.2.input.txt', make_result, make_found)
--- 4.Groups/4.1.GroupsMatch/util_4_1.py
+++ 4.Groups/4.1.GroupsMatch/util_4_1.py
@@ -1,11 +1,10 @@
 import re, sys
 
 
-def test(file_name, build_regex_func):
+def test(file_name, make_result_func, make_found_func, make_regex_func):
     sys.stdin = open(file_name, 'r', encoding='utf8')
     args_buffer = []
     args_count = 0
-    answers_total = 0
     answers_countdown = -1
     result = None
 
@@ -14,19 +13,23 @@ def test(file_name, build_regex_func):
         args_count += 1
 
         if args_count == 1:
-            result = re.findall(build_regex_func(), args_buffer.pop(0), 0)
+            result = make_result_func(make_found_func(make_regex_func, args_buffer.pop(0)))
+
         if args_count == 2:
+            answers_total = int(args_buffer[0])
+            answers_countdown = answers_total
+
             try:
-                answers_total = int(args_buffer[0])
-                answers_countdown = answers_total
                 assert len(result) == int(args_buffer.pop(0))
             except AssertionError:
                 print(f'{len(list(result))} not equal {answers_total}')
 
         if args_count > 2:
             answers_countdown -= 1
+            line = args_buffer[0]
+
             try:
-                line = args_buffer[0]
+
                 assert result.pop(0) == args_buffer.pop(0)
                 print(line, 'ok')
             except AssertionError:

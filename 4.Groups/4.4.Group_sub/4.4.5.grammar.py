194843e157db8c7c0f2098ee82ff758a52a7941c
gennady <genakoganovich@gmail.com>
2023-05-25 10:16:23 +0300

    0036: grammar with replacement function

--- 4.Groups/4.4.Group_sub/4.4.5.grammar.py
+++ 4.Groups/4.4.Group_sub/4.4.5.grammar.py
@@ -2,18 +2,23 @@ import re
 import util_4_4
 
 
-replace_dict = {r'[еЕ]гонный': r'[еЕ]го', r'[еЕ]ённый': r'[еЕ]ё', r'[иИ]хний': r'[иИ]х'}
-print(re.sub(r'[еЕ]гонный', r'[еЕ]го', 'Это был егонный билет.'))
-
-
 def make_regex():
-    return r'(\d{2}[./])(\d{2}[./])'
+    return r'([еЕ]гонны[йея])|([еЕ]ённы[йея])|([иИ]хни[йея])'
 
 
 def make_replacement(match_obj):
-    if match_obj.group(1) and match_obj.group(2):
-        return fr'{match_obj.group(2)}{match_obj.group(1)}'
+    if match_obj.group(1):
+        return fr'{match_obj.group(1)[0:3]}'
+    if match_obj.group(2):
+        return fr'{match_obj.group(2)[0:2]}'
+    if match_obj.group(3):
+        return fr'{match_obj.group(3)[0:2]}'
 
 
-print(util_4_4.make_result(make_regex(), make_replacement, input()))
+def make_result(regex, make_replacement_func, string):
+    return re.sub(regex, make_replacement_func, string)
+
+
+print(make_result(make_regex(), make_replacement, input()))
 # util_4_4.test('4.4.5.input.txt', util_4_4.make_result, make_regex(), make_replacement)
+

76b09b10a9bd1db25ba6c3d65cb4d32f02512937
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-05-31 22:50:27 +0300

    0067: 7.11.password_valid.py

--- 7.Offer/7.11.password_valid.py
+++ 7.Offer/7.11.password_valid.py
@@ -2,13 +2,33 @@ import re
 import util_7
 
 
-def make_pattern():
-    return re.compile(r'\w{6,}', re.I)
+def make_result(string):
+    return bool(re.search(r'(?=.*\d)', string)) \
+        and bool(re.search(r'(?=.*\w)', string)) \
+        and any(str(char).islower() for char in string) \
+        and any(str(char).isupper() for char in string) \
+        and bool(re.search(r'.{6,}', string)) and '_' not in string
 
 
-def make_result(pattern, string):
-    return str(bool(pattern.fullmatch(string)) and '_' not in string)
+print(make_result(input()))
 
 
-# print(make_result(make_pattern(), input()))
-util_7.test_vc('7.11.input.txt', make_result, make_pattern())
+# util_7.test_find_repeated('7.11.input.txt', make_result)
+
+
+def print_short_solution():
+    import regex
+
+    pattern = r'''
+    (?x)^(?=[^\W_]{6,})  # длина пароля более 6 символов
+    (?=.*?\d)  # определяю наличие цифры
+    (?=.*?[[:upper:]])  # определяю наличие заглавной буквы
+    (?=.*?[[:lower:]])  # определяю наличие маленькой буквы
+    [^\W_]+$  # записываю пароль правильными символами
+    '''.strip()
+    pattern = regex.compile(pattern)
+
+    print(bool(pattern.fullmatch(input())))
+
+
+print_short_solution()
